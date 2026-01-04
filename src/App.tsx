baf7486b03b963f8b89a02a6a111538f9aed34ee
Jannael Orlando <orlandojannael@gmail.com>
2026-01-04 15:33:46 -0600

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from 'react-router'
+import Login from './pages/Login'
+
 function App() {
-	return <div className="font-main font-light text-5xl">Init</div>
+	return (
+		<Routes>
+			<Route index></Route>
+			<Route element={<Login />} path='/login'></Route>
+			<Route path='/signup'></Route>
+			<Route path='/changeAccount'></Route>
+			<Route path='/createGroup'></Route>
+			<Route path='/forgotPwd'></Route>
+			<Route path='/groupInfo'></Route>
+		</Routes>
+	)
 }
 
 export default App
