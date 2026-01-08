b053c11e7827cf48f60ca3ffcfc5970a1c5fb404
Jannael Orlando <orlandojannael@gmail.com>
2026-01-08 10:56:15 -0600

    main buttons render

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,24 +1,48 @@
 import type { ReactNode } from 'react'
 import Page from '../components/ui/Page'
-import { DotsVertical } from '../icons'
+import { CubePlus, DotsVertical, MenuIcon } from '../icons'
 
 function Main() {
 	return (
-		<Page>
-			<GroupContainer>
-				<GroupItem color='#f21' name='insane group' />
-				<GroupItem color='#f21' name='insane group' />
-				<GroupItem color='#f21' name='insane group' />
-				<GroupItem color='#f21' name='insane group' />
-				<GroupItem color='#f21' name='insane group' />
-			</GroupContainer>
-		</Page>
+		<>
+			<Page
+				className='
+					p-10
+				'
+			>
+				<GroupContainer>
+					<GroupItem color='#f21' name='insane group' />
+					<GroupItem color='#f21' name='insane group' />
+					<GroupItem color='#f21' name='insane group' />
+					<GroupItem color='#f21' name='insane group' />
+					<GroupItem color='#f21' name='insane group' />
+					<GroupItem color='#f21' name='insane group' />
+					<GroupItem color='#f21' name='insane group' />
+					<GroupItem color='#f21' name='insane group' />
+					<GroupItem color='#f21' name='insane group' />
+					<GroupItem color='#f21' name='insane group' />
+					<GroupItem color='#f21' name='insane group' />
+					<GroupItem color='#f21' name='insane group' />
+					<GroupItem color='#f21' name='insane group' />
+				</GroupContainer>
+			</Page>
+			<ButtonsScreen />
+		</>
 	)
 }
 
+export default Main
+
 export function GroupContainer({ children }: { children: ReactNode }) {
 	return (
-		<div className='flex flex-wrap w-full gap-4 justify-evenly items-start max-h-dvh overflow-y-auto p-4'>
+		<div
+			className='
+				flex flex-wrap
+				w-full
+				p-4
+				gap-6 justify-evenly items-start
+			'
+		>
 			{children}
 		</div>
 	)
@@ -26,18 +50,97 @@ export function GroupContainer({ children }: { children: ReactNode }) {
 
 export function GroupItem({ name, color }: { name: string; color: string }) {
 	return (
-		<div className='flex-1 min-w-2xs bg-contrast h-24 relative'>
-			<button className='min-w-2xs h-24 flex w-full flex-col cursor-pointer' type='button'>
-				<div className='w-full h-14' style={{ background: color }}></div>
-				<div className='text-primary text-xl font-main flex-1 p-1'>
+		<div
+			className='
+				flex-1
+				min-w-2xs h-24
+				bg-contrast
+				rounded-2xl
+				shadow-sm shadow-contrast
+				relative
+			'
+		>
+			<button
+				className='
+					flex flex-col
+					min-w-2xs h-24 w-full
+					cursor-pointer
+				'
+				type='button'
+			>
+				<div
+					className='
+						w-full h-14
+						rounded-tl-2xl rounded-tr-2xl
+					'
+					style={{ background: color }}
+				></div>
+				<div
+					className='
+						flex-1
+						p-1
+						text-primary text-xl font-main
+					'
+				>
 					<p>{name}</p>
 				</div>
 			</button>
-			<button className='absolute left-0 top-3 cursor-pointer' type='button'>
+			<button
+				className='
+					p-1 m-1
+					border-contrast border-2 rounded-full
+					cursor-pointer
+					absolute left-0 top-0
+				'
+				type='button'
+			>
 				<DotsVertical />
 			</button>
 		</div>
 	)
 }
 
-export default Main
+export function ButtonsScreen() {
+	return (
+		<div
+			className='
+				flex
+				w-full min-h-dvh
+				text-contrast
+				fixed top-0 justify-center
+			'
+		>
+			<div
+				className='
+					max-w-7xl min-h-dvh w-full
+					fixed
+				'
+			>
+				<button
+					className='
+						w-10
+						m-2 p-1
+						border-contrast border-2 rounded-full
+						cursor-pointer
+						absolute right-0
+					'
+					type='button'
+				>
+					<MenuIcon />
+				</button>
+				<button
+					className='
+						w-10
+						m-2 p-1
+						border-contrast border-2 rounded-full
+						cursor-pointer
+						absolute right-0 bottom-0
+					'
+					type='button'
+				>
+					<CubePlus />
+				</button>
+			</div>
+		</div>
+	)
+}
