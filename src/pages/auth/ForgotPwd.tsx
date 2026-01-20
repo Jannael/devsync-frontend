337ad19aecf0d0a5cc685ddb51ac97cb7c9a7008
Jannael Orlando <orlandojannael@gmail.com>
2026-01-19 21:16:53 -0600

    refactor: forgotpwd page

--- src/pages/auth/ForgotPwd.tsx
+++ src/pages/auth/ForgotPwd.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import Form from '../../components/ui/Form'
 import FormButton from '../../components/ui/FormButton'
 import InputText from '../../components/ui/InputText'
@@ -6,46 +5,22 @@ import Label from '../../components/ui/Label'
 import P from '../../components/ui/P'
 import Page from '../../components/ui/Page'
 import Title from '../../components/ui/Title'
-import useRequestCodePwd from '../../hooks/auth/useRequestCodePwd'
-import useVerifyCodePwd from '../../hooks/auth/useVerifyCodePwd'
-import { localStorageKeys } from '../../localStorageKeys'
-import { routesConst } from '../../routes.constants'
-import VerifyAccount from '../../service/FormValidations/auth/VerifyAccount'
-import VerifyCodePassword from '../../service/FormValidations/auth/VerifyCodePassword'
+import useForgotPwdComponent from '../../hooks/components/useForgotPwd'
 
 function ForgotPwd() {
-	const [verifyCode, setVerifyCode] = useState(false)
-	const [error, setError] = useState<string | null>(null)
-
-	const { passwordVerifyCode } = useVerifyCodePwd(() => {
-		window.location.href = routesConst.login
-	})
-
-	const { passwordRequestCode } = useRequestCodePwd(() => {
-		setVerifyCode(true)
-	})
+	const {
+		verifyCode,
+		handleVerifyCodeSubmit,
+		handleRequestCodeSubmit,
+		error,
+		passwordRequestCode,
+		passwordVerifyCode,
+	} = useForgotPwdComponent()
 
 	return (
 		<Page className='flex justify-center items-center'>
 			{verifyCode && (
-				<Form
-					className='w-6/10 max-w-96'
-					onSubmit={(e) => {
-						const data = VerifyCodePassword(e, setError)
-						if (!data) return
-
-						const account = localStorage.getItem(
-							localStorageKeys.passwordVerifyCode,
-						)
-						if (account === null) return
-
-						passwordVerifyCode.mutate({
-							code: data.code.toString(),
-							account,
-							newPwd: data.newPwd.toString(),
-						})
-					}}
-				>
+				<Form className='w-6/10 max-w-96' onSubmit={handleVerifyCodeSubmit}>
 					<Title>Change password</Title>
 					<Label>
 						Code
@@ -68,20 +43,7 @@ function ForgotPwd() {
 				</Form>
 			)}
 			{!verifyCode && (
-				<Form
-					className='w-6/10 max-w-96'
-					onSubmit={(e) => {
-						const data = VerifyAccount(e, setError)
-						if (!data) return
-
-						localStorage.setItem(
-							localStorageKeys.passwordVerifyCode,
-							data.account.toString(),
-						)
-
-						passwordRequestCode.mutate({ account: data.account.toString() })
-					}}
-				>
+				<Form className='w-6/10 max-w-96' onSubmit={handleRequestCodeSubmit}>
 					<Title>Account</Title>
 					<Label>
 						Account
