17b800d995c234d44724593b922f44f5a3d12ae9
Jannael Orlando <orlandojannael@gmail.com>
2026-01-29 11:39:17 -0600

    chore: features section at /home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,16 @@
 import type { ReactNode } from 'react'
 import { Link } from 'react-router'
 import Page from '../../components/ui/Page'
-import { Github } from '../../icons'
+import { Check, Github } from '../../icons'
 import { routesConst } from '../../routes.constants'
 import Navbar from './components/Navbar'
 
 function Home() {
 	return (
-		<Page className='flex justify-center max-w-7xl'>
+		<Page className='flex justify-center max-w-7xl flex-col items-center'>
 			<Navbar />
 			<Section>
-				<main className='flex w-9/10'>
+				<main className='flex w-full justify-center items-center min-h-dvh'>
 					<div className='flex flex-col flex-1 justify-center gap-3'>
 						<a
 							className='h-fit w-fit mb-3'
@@ -27,13 +27,38 @@ function Home() {
 						<p className='text-xl text-contrast/80'>
 							The tools you need as a techLead
 						</p>
-						<Link className='border border-contrast px-5 py-3 rounded-full w-fit mt-5' to={routesConst.main}>
+						<Link
+							className='border border-contrast px-5 py-3 rounded-full w-fit mt-5'
+							to={routesConst.main}
+						>
 							Try here
 						</Link>
 					</div>
-
-					<img alt='A man with holding a taskBoard' className='flex-1' src='/HeroImg.webp' />
+					<img
+						alt='A man with holding a taskBoard'
+						className='flex-1'
+						src='/HeroImg.webp'
+					/>
 				</main>
+			</Section>
+			<Section>
+				<div className='size-full flex'>
+					<img
+						alt='taskList in the right with task details in the left'
+						className='flex-3 border-emerald-600 border-5 rounded-xl'
+						src='/GroupScreen.webp'
+					/>
+					<article className='flex-2 pl-10 flex flex-col justify-center'>
+						<h2 className='text-3xl mb-6 border-b-4 border-emerald-700 w-fit pb-3'>Features</h2>
+						<ul className='flex flex-col gap-4'>
+							<li className='flex [&>svg]:text-emerald-700/50 gap-3 text-xl'> <Check /> Code field</li>
+							<li className='flex [&>svg]:text-emerald-700/50 gap-3 text-xl'> <Check /> Features field</li>
+							<li className='flex [&>svg]:text-emerald-700/50 gap-3 text-xl'> <Check /> Solution tracking</li>
+							<li className='flex [&>svg]:text-emerald-700/50 gap-3 text-xl'> <Check /> Task management</li>
+							<li className='flex [&>svg]:text-emerald-700/50 gap-3 text-xl'> <Check /> User privileges by role</li>
+						</ul>
+					</article>
+				</div>
 			</Section>
 		</Page>
 	)
@@ -48,7 +73,7 @@ function Section({
 }) {
 	return (
 		<section
-			className={`${className} min-h-dvh flex flex-col items-center justify-center`}
+			className={`${className} flex flex-col items-center justify-center w-9/10`}
 		>
 			{children}
 		</section>
