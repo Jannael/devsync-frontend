6b314b427203f3ce7144935d0572b24e34f4130d
Jannael Orlando <orlandojannael@gmail.com>
2026-01-29 12:24:03 -0600

    chore: user management section

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,9 +3,36 @@ import { Link } from 'react-router'
 import Page from '../../components/ui/Page'
 import { Check, Github } from '../../icons'
 import { routesConst } from '../../routes.constants'
+import Li from './components/Li'
 import Navbar from './components/Navbar'
 
+const features = [
+	'Code field',
+	'Features field',
+	'Solution tracking',
+	'Task management',
+	'User privileges by role',
+]
+
+const userManagement = [
+	'Invite members',
+	'Remove members',
+	'Change role',
+	'Share group',
+]
+
 function Home() {
+	const featuresItems = features.map((feature) => (
+		<Li key={feature}>
+			<Check /> {feature}
+		</Li>
+	))
+	const userManagementItems = userManagement.map((feature) => (
+		<Li key={feature}>
+			<Check /> {feature}
+		</Li>
+	))
+
 	return (
 		<Page className='flex justify-center max-w-7xl flex-col items-center'>
 			<Navbar />
@@ -41,22 +68,40 @@ function Home() {
 					/>
 				</main>
 			</Section>
-			<Section>
-				<div className='size-full flex'>
+			<Section className='mb-[10dvh]'>
+				<div className='size-full flex mb-40'>
 					<img
 						alt='taskList in the right with task details in the left'
 						className='flex-3 border-emerald-600 border-5 rounded-xl'
 						src='/GroupScreen.webp'
 					/>
 					<article className='flex-2 pl-10 flex flex-col justify-center'>
-						<h2 className='text-3xl mb-6 border-b-4 border-emerald-700 w-fit pb-3'>Features</h2>
-						<ul className='flex flex-col gap-4'>
-							<li className='flex [&>svg]:text-emerald-700/50 gap-3 text-xl'> <Check /> Code field</li>
-							<li className='flex [&>svg]:text-emerald-700/50 gap-3 text-xl'> <Check /> Features field</li>
-							<li className='flex [&>svg]:text-emerald-700/50 gap-3 text-xl'> <Check /> Solution tracking</li>
-							<li className='flex [&>svg]:text-emerald-700/50 gap-3 text-xl'> <Check /> Task management</li>
-							<li className='flex [&>svg]:text-emerald-700/50 gap-3 text-xl'> <Check /> User privileges by role</li>
-						</ul>
+						<h2 className='text-3xl mb-6 border-b-4 border-emerald-700 w-fit pb-3'>
+							Features
+						</h2>
+						<ul className='flex flex-col gap-4'>{featuresItems}</ul>
+					</article>
+				</div>
+			</Section>
+			<Section>
+				<div className='size-full flex justify-center items-center mb-40'>
+					<picture className='flex-1 relative flex justify-center items-center'>
+						<img
+							alt='Menu with the option to create and join a group'
+							className='absolute bottom-0 right-0 z-100 -rotate-35 border-emerald-600 border-2 rounded-xl w-5/10'
+							src='/GroupMenu.webp'
+						/>
+						<img
+							alt='Create group form'
+							className='object-contain max-h-150 border-emerald-600 border-5 rounded-xl'
+							src='/CreateGroup.webp'
+						/>
+					</picture>
+					<article className='flex-1'>
+						<h2 className='text-3xl mb-6 border-b-4 border-emerald-700 w-fit pb-3'>
+							User Management
+						</h2>
+						<ul className='flex flex-col gap-4'>{userManagementItems} </ul>
 					</article>
 				</div>
 			</Section>
