0444d1076fc703d711e79d0cb2b65fcf92e0cd15
Jannael Orlando <orlandojannael@gmail.com>
2026-02-14 15:24:01 -0600

    feat: about sectio

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -13,7 +13,10 @@ const features = [
 function HomePage() {
 	const featureItems = features.map((feature) => {
 		return (
-			<li className='flex items-center gap-2 [&>svg]:text-shade text-2xl text-contrast/80' key={feature}>
+			<li
+				className='flex items-center gap-2 [&>svg]:text-shade text-2xl text-contrast/80'
+				key={feature}
+			>
 				<CheckIcon />
 				<p>{feature}</p>
 			</li>
@@ -64,7 +67,7 @@ function HomePage() {
 					</a>
 				</section>
 				<section
-					className='flex items-center justify-center my-10'
+					className='flex items-center justify-center'
 					id='Feature'
 				>
 					<div className='flex-1'>
@@ -78,10 +81,56 @@ function HomePage() {
 					</div>
 				</section>
 				<section
-					className='h-screen flex items-center justify-center'
+					className='my-40 flex items-center justify-center flex-col'
 					id='About'
 				>
-					<h2 className='text-6xl font-bold'>About Section</h2>
+					<h2 className='text-6xl font-bold mb-5'>About</h2>
+					<p className='text-2xl text-contrast/70 text-balance'>
+						Devsync was designed for personal improvement as developer, because
+						it's my first real project, the goal it's to maintain it and keep
+						the best experience for users with the tech-skills i have, while
+						keeping it open source, if you have suggestions, issues or want to
+						contribute please visit the{' '}
+						<a
+							className='text-accent hover:text-shade transition-all duration-300'
+							href='https://github.com/jannael/devsync'
+						>
+							repository
+						</a>
+						,
+						<br />
+						<br />
+						The page design, it is inspired by some open source pages i use,
+						such as{' '}
+						<a
+							className='text-accent hover:text-shade transition-all duration-300'
+							href='https://cubedesk.io'
+						>
+							CubeDesk,{' '}
+						</a>
+						<a
+							className='text-accent hover:text-shade transition-all duration-300'
+							href='https://npmx.dev'
+						>
+							Npmx{' '}
+						</a>{' '}
+						and{' '}
+						<a
+							className='text-accent hover:text-shade transition-all duration-300'
+							href='https://zustand-demo.pmnd.rs/'
+						>
+							zustand
+						</a>
+						, Illustrations are made by{' '}
+						<a
+							className='text-accent hover:text-shade transition-all duration-300'
+							href='https://www.instagram.com/nat.chavez18?utm_source=ig_web_button_share_sheet'
+							rel='noopener'
+							target='_blank'
+						>
+							Natali Chavez
+						</a>
+					</p>
 				</section>
 			</main>
 		</div>
