f677f606590850afb21903f6d2bf3c50df657fb4
Jannael Orlando <orlandojannael@gmail.com>
2026-02-14 22:25:44 -0600

    light mode colors

--- src/page/home/About.section.tsx
+++ src/page/home/About.section.tsx
@@ -37,7 +37,7 @@ function About() {
 export function Anchor({ href, name }: { href: string; name: string }) {
 	return (
 		<a
-			className='text-primary/70 hover:text-accent transition-all duration-300'
+			className='text-primary/80 hover:text-accent transition-all duration-300'
 			href={href}
 			rel='noopener'
 			target='_blank'
