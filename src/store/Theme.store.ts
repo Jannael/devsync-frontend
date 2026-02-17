0f1ad41f7af6ac377b2ce925b25fc9ef5c47cd59
Jannael Orlando <orlandojannael@gmail.com>
2026-02-16 20:00:17 -0600

    chore: colors to pick in appearance

--- src/store/Theme.store.ts
+++ src/store/Theme.store.ts
@@ -1,25 +1,60 @@
 import { create } from 'zustand'
 
+export const systemTheme = window.matchMedia('(prefers-color-scheme: dark)')
+	.matches
+	? 'dark'
+	: 'light'
+
+const defaultAccentColor = {
+	dark: '#00ffaa',
+	light: '#00ff80',
+}
+const defaultBgShade = {
+	dark: '#006142',
+	light: '#00ffae10',
+}
+const defaultPrimaryColor = {
+	dark: '#00af77',
+	light: '#03c584',
+}
+const defaultTheme = {
+	accentColor: defaultAccentColor[systemTheme],
+	bgShade: defaultBgShade[systemTheme],
+	primaryColor: defaultPrimaryColor[systemTheme],
+}
+
 export const accentColors = [
-	'#00ffaa',
-	'#00ff80',
-	'#222222',
-	'#333333',
-	'#444444',
+	defaultAccentColor.dark,
+	defaultAccentColor.light,
+	'#FF0055',
+	'#FF9100',
+	'#CCFF00',
+	'#00FFCC',
+	'#007BFF',
+	'#CC00FF',
+	'#FFFFFF',
 ] as const
 export const bgShades = [
-	'#006142',
-	'#00ffae10',
-	'#222222',
-	'#333333',
-	'#444444',
+	defaultBgShade.dark,
+	defaultBgShade.light,
+	'#22000B',
+	'#221300',
+	'#161C00',
+	'#001A14',
+	'#000B1A',
+	'#14001A',
+	'#111111',
 ] as const
 export const primaryColors = [
-	'#03c584',
-	'#00af77',
-	'#222222',
-	'#333333',
-	'#444444',
+	defaultPrimaryColor.dark,
+	defaultPrimaryColor.light,
+	'#D00043',
+	'#E67E00',
+	'#A3CC00',
+	'#00CC99',
+	'#0056B3',
+	'#A300CC',
+	'#888888',
 ] as const
 
 interface ThemeStore {
@@ -33,25 +68,15 @@ interface ThemeStore {
 	setPrimaryColor: (color: (typeof primaryColors)[number]) => void
 }
 
-export const systemTheme = window.matchMedia('(prefers-color-scheme: dark)')
-	.matches
-	? 'dark'
-	: 'light'
 const currentAccentColor = localStorage.getItem('accentColor')
 	? localStorage.getItem('accentColor')
-	: systemTheme === 'dark'
-		? '#00ffaa'
-		: '#00ff80'
+	: defaultTheme.accentColor
 const currentBgShade = localStorage.getItem('shade')
 	? localStorage.getItem('shade')
-	: systemTheme === 'dark'
-		? '#006142'
-		: '#00ffae10'
+	: defaultTheme.bgShade
 const currentPrimaryColor = localStorage.getItem('primaryColor')
 	? localStorage.getItem('primaryColor')
-	: systemTheme === 'dark'
-		? '#00af77'
-		: '#03c584'
+	: defaultTheme.primaryColor
 
 const useThemeStore = create<ThemeStore>((set) => ({
 	isDarkTheme: systemTheme === 'dark', // for images constants
