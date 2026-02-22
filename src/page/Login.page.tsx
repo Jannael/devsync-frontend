2f3a063c75624786c953252a24d6ace381c1f24c
Jannael Orlando <orlandojannael@gmail.com>
2026-02-21 21:05:21 -0600

    fix: login layout

--- src/page/Login.page.tsx
+++ src/page/Login.page.tsx
@@ -14,9 +14,9 @@ const ForgotPassword = lazy(
 function Login() {
 	const { show } = useLoginStore()
 	return (
-		<div className='h-screen bg-main flex justify-center items-center text-txt p-4'>
+		<div className='min-h-dvh bg-main flex justify-center items-center text-txt p-4'>
 			<Toaster />
-			<main className='flex flex-col lg:flex-row size-full max-w-7xl gap-10 lg:gap-0'>
+			<main className='flex flex-col lg:flex-row size-full max-w-7xl gap-10 lg:gap-0 items-center'>
 				<div className='flex-1 flex flex-col justify-center items-center gap-5 text-center'>
 					<div className='w-7/10'>
 						<Pet />
